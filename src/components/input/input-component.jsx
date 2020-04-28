e5fc2cdc4ca8049e66483fa1d5cc4d6d4f8878f0
Udayskai <udayskai@gmail.com>
2020-04-29 00:12:53 +0530

    add input, P - api fetch and adding component

---
+++ src/components/input/input-component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Input extends Component {
+    render() {
+        return (
+            <div>
+                Input try to add style in same page
+            </div>
+        )
+    }
+}
